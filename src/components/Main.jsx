535a0f89130047510f641d9502b3b0b2048c5585
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-07-17 13:46:56 +0200

    add video if img doen't exist

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,19 @@ export default function Main(props) {
     const {data} = props
 
     return (
-        <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || 'bg_img'} className="bg_image"/>
+        <div className="mediaContainer">
+            {data.media_type === 'video' ? (
+                <iframe 
+                    src={data?.url} 
+                    title={data?.title || 'bg_video'} 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    className="bg_video"
+                    allowFullScreen
+                    frameBorder="0">
+                </iframe>
+            ) : (
+                <img src={data.url || data.hdurl} alt={data.title || 'bg_img'} className="bg_image"/>
+            )}
         </div>
     )
 }
