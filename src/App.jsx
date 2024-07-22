ffc45d7c7c1afe34df1206c0f10ce598188698e0
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-07-22 11:20:59 +0200

    change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,13 @@
-import { useEffect, useState } from "react"
-import TitleAPOD from "./components/TitleAPOD"
 import APOD from "./components/APOD"
-import SideBarAPOD from "./components/SideBarAPOD"
 import NeoWs from "./components/NeoWs"
 import EPIC from "./components/EPIC"
 
 function App() {
-  const [data, setData] = useState(null)
-  // const [loading, setLoading] = useState(false)
-  const [showModal, setShowModal] = useState(false)
-
-  function handleToggleModal() {
-    setShowModal(!showModal)
-  }
-
-  useEffect(() => {
-    async function fetchAPIData() {
-      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
-
-      const today = (new Date()).toDateString()
-      const localKey = `NASA-${today}`
-      const localData = localStorage.getItem(localKey)
-
-      if(localData) {
-        try{
-          const apiData = JSON.parse(localData)
-          setData(apiData)
-          console.log("Fetched from cache today")
-          return;
-        } catch (err) {
-          console.error("Failed to parse cache data", err)
-          localStorage.removeItem(localKey)
-        }
-      }
-
-      localStorage.clear()
-
-      try {
-          const res = await fetch(url)
-          const apiData = await res.json()
-          localStorage.setItem(localKey, JSON.stringify(apiData))
-          setData(apiData)
-          console.log("Fetched from API today")
-      } catch(err) {
-         console.log(err.message)
-      }
-    }
-    fetchAPIData()
-  }, [])
 
   return (
     <>
       <section className="section_APOD">
-        {data && (
-          <TitleAPOD data={data} handleToggleModal={handleToggleModal} />
-        )}
-        { data ? (<APOD data={data}/>) : (
-          <div className="loadingState">
-            <i className="fa-solid fa-gear"></i>
-          </div>
-        )}
-        {showModal && (
-            <SideBarAPOD data={data} handleToggleModal={handleToggleModal}/>
-        )}
+        <APOD />
       </section>
       <section className="section_NeoWs">
         <NeoWs/>
--- src/components/APOD.jsx
+++ src/components/APOD.jsx
@@ -1,10 +1,76 @@
-/* eslint-disable react/prop-types */
-export default function APOD(props) {
+import { useEffect, useState } from "react";
+import TitleAPOD from "./TitleAPOD";
+import SideBarAPOD from "./SideBarAPOD";
+import 'react-tooltip/dist/react-tooltip.css';
+// import { Tooltip } from 'react-tooltip';
 
-    const {data} = props
+/* eslint-disable react/prop-types */
+export default function APOD() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [showModal, setShowModal] = useState(false)
+
+    function handleToggleModal() {
+      setShowModal(!showModal)
+    }
+
+     useEffect(() => {
+        async function fetchAPIData() {
+            const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+            const today = (new Date()).toDateString()
+            const localKey = `NASA-${today}`
+            const localData = localStorage.getItem(localKey)
+
+            if (localData) {
+                try {
+                    const apiData = JSON.parse(localData)
+                    setData(apiData)
+                    console.log("Fetched from cache today")
+                    setLoading(false)
+                    return
+                } catch (err) {
+                    console.error("Failed to parse cache data", err)
+                    localStorage.removeItem(localKey)
+                }
+            }
+
+            localStorage.clear()
+
+            try {
+                const res = await fetch(url)
+                const apiData = await res.json()
+                localStorage.setItem(localKey, JSON.stringify(apiData))
+                setData(apiData)
+                console.log("Fetched from API today")
+            } catch (err) {
+                console.log(err.message)
+                setError(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchAPIData()
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="loadingState">
+                <i className="fa-solid fa-gear fa-spin"></i>
+            </div>
+        );
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     return (
         <div className="mediaContainer">
+         <TitleAPOD data={data} handleToggleModal={handleToggleModal} />
             {data.media_type === 'video' ? (
                 <iframe 
                     src={data?.url} 
@@ -17,6 +83,7 @@ export default function APOD(props) {
             ) : (
                 <img src={data.url || data.hdurl} alt={data.title || 'bg_img'} className="bg_image"/>
             )}
+            {showModal && <SideBarAPOD data={data} handleToggleModal={handleToggleModal} />}
         </div>
     )
 }
